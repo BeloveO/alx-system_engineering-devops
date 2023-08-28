1668c665084f1fb0bb94288ab793d9b08fee03f1
Belove <beloveolusola@gmail.com>
2023-08-28 03:38:06 +0100

    Puppet

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -3,14 +3,14 @@ include stdlib
 
 file_line { 'No passwd auth':
   ensure => 'present',
-  path   => 'etc/ssh/ssh_config',
+  path   => '/etc/ssh/ssh_config',
   line   => 'PasswordAuthentication no',
   match  => '^PasswordAuthentication'
 }
 
 file_line { 'Identity_file':
   ensure => 'present',
-  path   => 'etc/ssh/ssh_config',
+  path   => '/etc/ssh/ssh_config',
   line   => 'IdentityFile ~/.ssh/school',
   match  => '^IdentityFile'
 }
