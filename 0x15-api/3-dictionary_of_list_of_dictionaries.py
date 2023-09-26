ec3154d76278be6a2d6afed1e59eb915593f3c21
Belove <beloveolusola@gmail.com>
2023-09-26 03:49:28 +0100

    Dict of dict

--- 0x15-api/3-dictionary_of_list_of_dictionaries.py
+++ 0x15-api/3-dictionary_of_list_of_dictionaries.py
@@ -9,24 +9,16 @@ import sys
 if __name__ == "__main__":
     url = 'https://jsonplaceholder.typicode.com/'
 
-    user = '{}users/{}'.format(url, sys.argv[1])
+    user = '{}users/'.format(url)
     res = requests.get(user)
     json_o = res.json()
-    d_task = {}
-    for user in json_o:
-        name = json_o.get('username')
-        userid = user.get('id')
-        todos = '{}todos?userId={}'.format(url, userid)
-        res = requests.get(todos)
-        tasks = res.json()
-        l_task = []
-        for task in tasks:
-            tdict = {"username": name,
-                     "task": task.get('title'),
-                     "completed": task.get('completed')}
-            l_task.append(tdict)
-
-        d_task[str(userid)] = l_task
-    file = 'todo_all_employees.json'
-    with open(file, mode='w') as f:
-        json.dump(d_task, f)
+    with open("todo_all_employees.json", "w") as file:
+        json.dump({
+            u.get("id"): [{
+                "task": t.get("title"),
+                "completed": t.get("completed"),
+                "username": u.get("username")
+            } for t in requests.get(url + "todos",
+                                    params={"userId": u.get("id")}).json()]
+            for u in json_o}, file)
+    
