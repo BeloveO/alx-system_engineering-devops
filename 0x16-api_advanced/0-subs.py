d62a7435bb1716a87b83e3253420384212284d75
Belove <beloveolusola@gmail.com>
2023-10-12 03:31:53 +0100

    Subscribers

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and returns the number of subscribers
+"""
+import requests
+import sys
+
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{subreddit}/about.json".format(
+        subreddit=subreddit)
+    user_agent = {'User-Agent': 'Python/requests'}
+    res = requests.get(url, headers=user_agent, allow_redirects=False)
+    if res.status_code in [302, 404]:
+        return 0
+    return res
